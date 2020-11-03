e141f58698ff5fd2d30dc7e5a547187d470d721d
Hiram Fleitas <hfleitas@universalproperty.com>
2020-11-03 15:35:46 -0500

    fixed srm.sql

--- srm.sql
+++ srm.sql
@@ -24,48 +24,36 @@ create table Inventory (
     InventoryID int identity primary key clustered,
     ProductCode int foreign key references Product(ProductCode),
     Location varchar(128),
-    Shelf varchar(25),
-    ItemCount int,
-    BoxCount int,
-    PalletCount int,
-    ContainerCount int,
-    Total int,
+    Shelf varchar(25), 
+    SquareFeet int,
     CreatedDate datetime default(getdate())
 );
 go 
 set identity_insert Product on; 
 
-insert Product ([Code], [Name], [Description], [Orgin], [Manufacture], [Size], [Price], [Cost])
+insert Product ([ProductCode], [Name], [Description], [Orgin], [Manufacture], [Size], [Price], [Cost])
 values
     (2350138, 'Hamlet Blanco', 'Glossy marble with snowy gray-whites undertones', 'Spain', 'SRM', '48x48', 3.53, .99*rand()),
     (2350099, 'Toscana Blanco', 'Glossy marble with white undertones and slight present gray tones', 'Spain', 'Geotiles', '36x36', 2.53, .99*rand()),
     (2350098, 'Salon Wiskey', 'Glossy marble with warm white undertone open balance gray-golds', 'Italy', 'Ferrari', '32x32', 2, .99*rand()),
-
     (24483052, 'Catalatta Gold', 'Glossy marble cream with minimal gray tone', 'Spain', 'Ecoceramic', '24x48', 2.89, .99*rand()),
     (24483051, 'Calacata Trento Gulk', 'Glossy marble light grays with minimal gray tone', 'India', 'Margrette', '24x48', 1.67, .99*rand()),
     (24483053, 'India Candela', 'Glossy marble white and gray very minimal', 'India', 'MGH', '24x48', 1.89, .99*rand()),
-
     (32323054, 'Snow Gray India', 'Glossy marble gray snowy', 'India', 'Pan Caliente', '32x32', 1.39, .99*rand()),
     (32323055, 'Statuario Elegant Polish', 'Glossy marble minimal white gray blues', 'India', 'Ella', '32x32', 1.89, .99*rand())
 
 set identity_insert Product off; 
 go 
-insert Inventory (ProductID, Location, Shelf, ItemCount, BoxCount, PalletCount, ContainerCount, Total, CreatedDate)
-values
-(2350138, 'Hamlet Blanco', 'Glossy marble with snowy gray-whites undertones', 'Spain', 'SRM', '48x48', 3.53, .99*rand()),
-(2350099, 'Toscana Blanco', 'Glossy marble with white undertones and slight present gray tones', 'Spain', 'Geotiles', '36x36', 2.53, .99*rand()),
-(2350098, 'Salon Wiskey', 'Glossy marble with warm white undertone open balance gray-golds', 'Italy', 'Ferrari', '32x32', 2, .99*rand()),
-
-(24483052, 'Catalatta Gold', 'Glossy marble cream with minimal gray tone', 'Spain', 'Ecoceramic', '24x48', 2.89, .99*rand()),
-(24483051, 'Calacata Trento Gulk', 'Glossy marble light grays with minimal gray tone', 'India', 'Margrette', '24x48', 1.67, .99*rand()),
-(24483053, 'India Candela', 'Glossy marble white and gray very minimal', 'India', 'MGH', '24x48', 1.89, .99*rand()),
-
-(32323054, 'Snow Gray India', 'Glossy marble gray snowy', 'India', 'Pan Caliente', '32x32', 1.39, .99*rand()),
-(32323055, 'Statuario Elegant Polish', 'Glossy marble minimal white gray blues', 'India', 'Ella', '32x32', 1.89, .99*rand())
+insert Inventory (ProductCode, Location, Shelf, SquareFeet)
+select 2350138, 'Doral', substring(convert(varchar(40),newid()),1,3), round(rand()*20000,0)+2700 union
+select 2350099, 'Doral', substring(convert(varchar(40),newid()),1,3), round(rand()*20000,0)+2700 union
+select 2350098, 'Doral', substring(convert(varchar(40),newid()),1,3), round(rand()*30000,0)+1000 union
+select 24483052, 'Hialeah', substring(convert(varchar(40),newid()),1,3), round(rand()*30000,0)+1000 union
+select 24483051, 'Hialeah', substring(convert(varchar(40),newid()),1,3), round(rand()*10000,0)+1000 union
+select 24483053, 'Hialeah', substring(convert(varchar(40),newid()),1,3), round(rand()*20000,0)+1000 union
+select 32323054, 'Warehouse', substring(convert(varchar(40),newid()),1,3), round(rand()*10000,0)+1000 union
+select 32323055, 'Warehouse', substring(convert(varchar(40),newid()),1,3), round(rand()*10000,0)+1000 
 go 
 select * from Product
 select * from Inventory
 go 
-
-
-SELECT SUBSTRING(CONVERT(varchar(40), NEWID()),1,3)
